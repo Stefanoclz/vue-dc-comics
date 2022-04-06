f37dcc92f36b8d25cb20c74a0023986a072c3221
Stefano Calzoni <stefanoclz@hotmail.it>
2022-04-06 15:29:16 +0200

    aggiunti primi elementi all'header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,39 @@
 <template>
-  <header></header>
+  <header>
+    <img src="../assets/dc-logo.png" alt="DC logo" />
+    <ul>
+      <li>
+        <a href="#">CHARACTERS</a>
+      </li>
+      <li>
+        <a href="#">COMICS</a>
+      </li>
+      <li>
+        <a href="#">MOVIES</a>
+      </li>
+      <li>
+        <a href="#">TV</a>
+      </li>
+      <li>
+        <a href="#">GAMES</a>
+      </li>
+      <li>
+        <a href="#">COLLECTIBLES</a>
+      </li>
+      <li>
+        <a href="#">VIDEOS</a>
+      </li>
+      <li>
+        <a href="#">FANS</a>
+      </li>
+      <li>
+        <a href="#">NEWS</a>
+      </li>
+      <li>
+        <a href="#">SHOP</a>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <script>
@@ -8,5 +42,33 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+  background-color: white;
+  padding: 20px;
+
+  img {
+    height: 80px;
+  }
+
+  ul {
+    display: flex;
+    column-gap: 20px;
+    align-items: center;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+    font-size: 13px;
+  }
+}
 </style>
